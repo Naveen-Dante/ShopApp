4b2c9a0fa32da8462b4dc7128bdd9bc2c3c64cbf
Naveen-Dante <naveenkumar151294@gmail.com>
2017-01-18 23:50:02 +0530

    Fourth time Changes in port data and connector

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ app.use(session({
         host: '127.0.0.1',
         port: '27017',
         db: 'shop',
-        /*url: 'mongodb://localhost:27017/shop'*/
+        url: 'mongodb://localhost:27017/shop'
         }),
     cookie:{maxAge: 180*60*1000}
 }));
